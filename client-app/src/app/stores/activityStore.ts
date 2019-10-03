4799bafc0e825d83932eefa488830a4b4fb034d2
Justin Gallow <justingallow@Justins-MacBook-Pro.local>
2019-10-03 16:53:00 -0600

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,20 @@ export class ActivityStore {
     @observable target = '';
 
     @computed get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort(
-            (a, b) => Date.parse(a.date) - Date.parse(b.date))
-    };
+        console.log(this.groupActivitiesByDate(Array.from(this.activityRegistry.values())))
+        return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()));
+    }
+
+    groupActivitiesByDate(activities: IActivity[]) {
+        const sortedActivies = activities.sort(
+            (a, b) => Date.parse(a.date) - Date.parse(b.date)
+        )
+        return Object.entries(sortedActivies.reduce((activities, activity) => {
+            const date = activity.date.split('T')[0];
+            activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+            return activities;
+        }, {} as {[key: string]: IActivity[]}));
+    }
 
     @action loadActivities = async () => {
         this.loadingInitial = true;
@@ -27,7 +38,8 @@ export class ActivityStore {
                     this.activityRegistry.set(activity.id, activity);
                 });
                 this.loadingInitial = false;
-            })
+            });
+            console.log(this.groupActivitiesByDate(activities));
         } catch (error) {
             runInAction('load activities error', () => {
                 this.loadingInitial = false;
